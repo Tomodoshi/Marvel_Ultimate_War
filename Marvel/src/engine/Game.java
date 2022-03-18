b0d5d29315d09ba62fa3a3f48112d4a78d88dc15
standsuser <34959945+standsuser@users.noreply.github.com>
2022-03-18 22:06:47 +0200

    Add files via upload

---
+++ Marvel/src/engine/Game.java
@@ -0,0 +1,12 @@
+package engine;
+import model.world.Champion;
+public class Game {
+	private Player firstPlayer;
+	private Player secondPlayer;
+	private boolean firstLeaderAbilityUsed;
+	private boolean secondLeaderAbilityUsed;
+	private Object[][] board = new Object[5][5];
+	static ArrayList<Champion> availableChampions;
+	
+	
+}
